fd7acf2360ed1b84788987eca806ff6408e76072
minhaj6006 <72701898+minhaj6006@users.noreply.github.com>
2020-10-22 03:02:01 +0500

    modifide

--- library management system/LMS.py
+++ library management system/LMS.py
@@ -11,6 +11,7 @@
 # server.quit()
 # print("sent!")
 ############################################################################
+from pathlib import Path
 available_books = {"HP1" : "Harry Potter",
                    "TTS" : "The Twilight Saga",
                    "TLR" : "The Lord of the Rings",
@@ -32,16 +33,32 @@ book_index_mapping = {1: "HP1",
 print('*************************************')
 print('*     Library Management System     *')
 print('*************************************')
-ask_email = input('Register your self by email: ')
-emai_data=[ask_email] #store user email
-select_books=[]
+ask_name = input('\nRegister your self by name: ')
+ask_email = input('enter your email: ')
+name_data = [ask_name] # store user naem
+emai_data = [ask_email] #store user email
+select_books = []
 
+
+if Path('{0}.txt'.format(name_data[0])).is_file(): # checking file exist
+    file = open('{0}.txt'.format(name_data[0]),"r") #{0} using for read file name by username
+    select_books = [line.rstrip('\n') for line in open(name_data[0] + ".txt" )] # this point text file convert into list
+    print('\n--------------------')
+    print('Already Registered')
+    print('Welcome Back ' + ask_name)
+    print('--------------------\n')
+
+else:
+    file = open('{0}.txt'.format(name_data[0]),"w") #{0} using for write file name by username
+    print('\n---------------')
+    print('Welcome  ' + ask_name)
+    print('---------------\n')
 
 b=True
 while (b):
     print('\n************ Main Menu ************')
     print('-----------------------------------')
-    print('1. Select Book. \n2. Drop Book. \n3. .\n4. Exit Program.')
+    print('1. Checkout Book. \n2. Return Book. \n3. Show rented Books .\n4. Exit Program.')
     print('-----------------------------------')
     option1 = '1'
     option2 = '2'
@@ -57,10 +74,41 @@ while (b):
             print('----------------------------')
             break
         else:
+            file = open('{0}.txt'.format(name_data[0]),"a")
             count = 0
+            print('\n    Available Books ')
+            print('-----------------------')
             for i in available_books: # this loop using for add numbering
                 count += 1
                 print(f"{count}" + " " + available_books[i])
+            print('-----------------------\n')
+
+            b_select = int(input('Select the book: '))
+
+            if (b_select == 1 and book_index_mapping[1] not in select_books):
+                file.write(str("%s\n" % book_index_mapping[1]))
+                select_books.append(book_index_mapping[1])
+                print('rent' + book_index_mapping[1])
+            elif (b_select == 1 and book_index_mapping[1] in select_books):
+                print('\nYou are already rented this book')
+
+
+
+# option3 Show rent book
+    elif choose_option == option3:
+        s_books = [line.rstrip('\n') for line in (select_books)]
+        count = 0
+        print('\nYou rented these books.')
+        print('-----------------------')
+        for i in s_books: # this loop using for add numbering
+            count += 1
+            print(' '.join([str(count),i]) )
+        print('-----------------------')
+        r = (int(input("\npress 1 to return Main Menu and press 0 to exit: ")))
+        if r == 1:
+            continue
+        elif r == 0:
+            break
 
 # option 4 exit
     elif choose_option == option4: # this point exit program
